b4e143b2ee25976b3f40e428b451d5f40050229e
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-04-17 13:46:31 -0600

    Adding a Postgres service

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,17 @@
-import main
+package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/sdGuille/trivia-go/database"
+)
 
 func main() {
+	database.ConnectDb()
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello World!")
+		return c.SendString("Hello Mom i'm Using docker and go, how cool is that!")
 	})
 
 	app.Listen(":3000")
