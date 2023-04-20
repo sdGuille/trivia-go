059b9ed92f6d1272fd4d365186e9560d6e11e8a4
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-04-19 21:06:54 -0600

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
 	"github.com/sdGuille/trivia-go/database"
 )
 
 func main() {
 	database.ConnectDb()
-
-	app := fiber.New()
+	
+	engine := html.New("./views", "./html")
+	
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
 	setupRoutes(app)
 
